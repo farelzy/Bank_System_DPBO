7b0a673ee6e7f2a2685221cb90d0d3ee1e8997a3
DuckyDash <andikasutarni@gmail.com>
2024-12-27 09:15:21 +0700

    Bank

---
+++ src/main/java/Bank/Customer.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Bank;
+
+/**
+ *
+ * @author msi_k
+ */
+public abstract class Customer {
+    private String name;
+    private String id;
+    
+    public Customer(String name, String id){
+        this.name = name;
+        this.id = id;
+    }
+    
+    public String getName(){
+        return name;
+    }
+    
+    public String getId(){
+        return id;
+    }
+}
---
+++ src/main/java/Bank/IBankService.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package Bank;
+
+/**
+ *
+ * @author msi_k
+ */
+public interface IBankService {
+    public void deposit(double amount);
+    public void withdraw(double amount);
+    public void getBalance();
+}
